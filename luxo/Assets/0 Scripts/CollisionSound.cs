680d908a9a097ed9cdfdfe6682d732f3035dc3ec
Raymond Huang <huang.ra@northeastern.edu>
2018-10-14 05:30:05 -0400

    added animations for couch, alfredo, floor lamp

---
+++ luxo/Assets/0 Scripts/CollisionSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour {
+    public AudioClip[] clips;
+
+    private AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("add sounds to beer!!");
+        source = GetComponent<AudioSource>();
+	}
+
+    public void OnCollisionEnter(Collision other) {
+        // if soft, don't make noise
+        if (other.gameObject.GetComponentInParent<SoftObject>()) {
+            return;
+        }
+
+        source.PlayOneShot(clips[Random.Range(0, clips.Length)], 1f);
+    }
+}
