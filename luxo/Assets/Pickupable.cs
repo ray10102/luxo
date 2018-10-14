e1ee388f798dac86b1f92c151c167a15cbaf25f6
Raymond Huang <huang.ra@northeastern.edu>
2018-10-14 02:33:48 -0400

    added lighting controls, object physics, dev camera, deformed terrain, hid unseeable objects

---
+++ luxo/Assets/Pickupable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.LogWarning("// TODO: Resize controller collider");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
